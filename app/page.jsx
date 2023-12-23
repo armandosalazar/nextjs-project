bfeca971690389cd8450f3fc602b00aefa240ef8
Armando Salazar <armandosalazar351@gmail.com>
2023-12-22 22:46:39 -0600

    refactor: Refactor home page component to use Next.js Link and add localStorage functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
 export default function HomePage() {
   return (
-    <>
+    <section>
       <h1>Home Page</h1>
-      <a href="/x">Not Found</a>
-    </>
+      <Link href="/x">Not Found</Link>
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          alert("hello");
+          window.localStorage.setItem("test", "hello");
+        }}
+      >
+        Click
+      </button>
+      {window.localStorage.getItem("test") ? (
+        <p>localStorage: {window.localStorage.getItem("test")}</p>
+      ) : (
+        <p>localStorage: null</p>
+      )}
+    </section>
   );
 }
