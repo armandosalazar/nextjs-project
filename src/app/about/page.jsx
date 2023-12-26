c14f1eca9df860261f096e3a7c4f35712a262d12
Armando Salazar <armandosalazar351@gmail.com>
2023-12-25 18:06:48 -0600

    refactor: Refactor AboutPage component and add navigation functionality

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,30 @@
-export const metadata = {
-  title: "About Us",
-};
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function AboutPage() {
+  const router = useRouter();
+
   return (
-    <div className="m-4">
-      <h1>About</h1>
-    </div>
+    <main className="m-4">
+      <h1 className="text-3xl my-5">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+        similique deleniti necessitatibus ducimus voluptatem tenetur error
+        cumque reprehenderit odit magnam eos, facilis eum provident aut sequi
+        laudantium laboriosam quidem vel.
+      </p>
+
+      <button
+        className="bg-sky-400 py-2 px-3 rounded-md text-white my-2"
+        onClick={() => {
+          // Execute logic before leaving the page
+          alert("You are leaving the page");
+          // Get data from the server
+          router.push("/");
+        }}
+      >
+        Go to Home
+      </button>
+    </main>
   );
 }
